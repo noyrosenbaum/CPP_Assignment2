b4d5f10d54cb78ea9ebbe2a19eeea89f50763fdc
noyrosenbaum <noyrosen123@gmail.com>
2023-03-28 21:52:51 +0300

    Sketch of Tests

--- Test.cpp
+++ Test.cpp
@@ -40,6 +40,7 @@ TEST_CASE("Test Player Class")
 {
     Player p1("Alice");
     Player p2("Bob");
+    REQUIRE(p1.cardesTaken() >= 0);
     REQUIRE(p2.cardesTaken() >= 0);
     /**
      * Player - tests
@@ -53,15 +54,22 @@ TEST_CASE("Test Player Class")
     SUBCASE("Test player name")
     {
         CHECK_EQ(p1.getName(), "Alice");
+        CHECK_EQ(p2.getName(), "Bob");
     }
 
     SUBCASE("Test player initial stack size")
     {
+        CHECK_EQ(p1.stacksize(), 26);
         CHECK_EQ(p2.stacksize(), 26);
     }
     SUBCASE("Test player final number of cards when there was no change")
     {
+        CHECK_EQ(p1.cardesTaken(), 0);
         CHECK_EQ(p2.cardesTaken(), 0);
+    }
+    SUBCASE("Test player total cards combining 2 players")
+    {
+        CHECK_EQ(p1.stacksize() + p1.cardesTaken() + p2.stacksize() + p2.cardesTaken(), 52);
     }
 
     // SUBCASE("Test player play card")
@@ -91,53 +99,63 @@ TEST_CASE("Test Game Class")
     Player p1("Alice");
     Player p2("Bob");
     Game game(p1, p2);
+    int i;
 
     SUBCASE("Test game initialization")
     {
-        CHECK_EQ(game.getTurnNumber(), 0);
+        CHECK_THROWS_WITH(Game(p1, p1), "A game contains 2 individuals");
     }
 
     SUBCASE("Test game play turn")
     {
-        game.playTurn();
-        CHECK_GT(p1.cardesTaken() + p2.cardesTaken(), 0);
-        CHECK_EQ(game.getTurnNumber(), 1);
+        SUBCASE("Test no war")
+        {
+            while (i++ < 5)
+            {
+                game.playTurn();
+                CHECK_EQ(p1.cardesTaken() + p2.cardesTaken(), 0);
+                CHECK_EQ(p1.stacksize() + p2.stacksize(), 42);
+            }
+        }
+        SUBCASE("Test player has 0 cards after a war")
+        {
+            while (i++ < 5)
+            {
+                game.playTurn();
+                CHECK_EQ(p1.cardesTaken() + p2.cardesTaken(), 26);
+                CHECK_EQ(p1.stacksize() + p2.stacksize(), 52);
+            }
+        }
     }
 
     SUBCASE("Test game play all")
     {
         game.playAll();
-        CHECK_GT(p1.cardesTaken() + p2.cardesTaken(), 0);
-        CHECK_EQ(game.getTurnNumber(), 26);
+        CHECK_EQ(p1.cardesTaken() || p2.cardesTaken(), 26);
+        CHECK_EQ(p1.stacksize() || p2.stacksize(), 0);
     }
 
     SUBCASE("Test game winner")
     {
         game.playAll();
-        CHECK_NE(game.getWinner(), nullptr);
+        CHECK_THROWS(game.printWiner());
     }
 
     SUBCASE("Test game stats")
     {
         game.playAll();
-        game.printStats();
+        CHECK_THROWS(game.printStats());
     }
 
     SUBCASE("Test game log")
     {
         game.playAll();
-        CHECK_GT(game.getLog().size(), 0);
+        CHECK_THROWS(game.printLog());
     }
 
     SUBCASE("Test game last turn stats")
     {
         game.playAll();
-        CHECK(game.getLastTurn().size() > 0);
-    }
-
-    SUBCASE("Test player stack size after playing")
-    {
-        game.playAll();
-        CHECK_EQ(p1.stacksize() + p2.stacksize(), 0);
+        CHECK_THROWS(game.printLastTurn());
     }
 }
